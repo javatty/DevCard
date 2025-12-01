b1bf6cc324d6bfac712260ce2315a3ca590ff51b
Javad <0hashemi1@gmail.com>
2025-12-02 00:13:14 +0330

    project section from index page moved to partial  view project

---
+++ DevCard_MVC/Models/IndexModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DevCard_MVC.Models
+{
+    public class IndexModel
+    {
+        public List<Project> projects { get; set; }
+        public List<Article> articles { get; private set; }
+    }
+}
