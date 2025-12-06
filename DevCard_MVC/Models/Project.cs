1ad0efb17cc6c6ea523b86b5874f1ddd70dbff2d
Javad <0hashemi1@gmail.com>
2025-12-06 20:56:28 +0330

    Projects & Latest Articles Moved to View Component

--- DevCard_MVC/Models/Project.cs
+++ DevCard_MVC/Models/Project.cs
@@ -2,9 +2,22 @@
 {
     public class Project
     {
+        public Project(int id,string name,string description,string image, string client)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Image = image;
+            Client = client;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
         public string Client { get; set; }
+
+
     }
+
 }
